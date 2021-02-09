837c304e4fdf8d7048190c7a2d84065d05328068
Kevin Lefevre <lefevre.kevin@gmail.com>
2021-02-09 11:39:33 +0100

    feat: update submodule script
    Signed-off-by: Kevin Lefevre <lefevre.kevin@gmail.com>

---
+++ update-submodule.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+
+git fetch istio master git subtree pull --prefix istio istio master --squash
+git fetch aws-ebs-csi-driver master git subtree pull --prefix aws-ebs-csi-driver aws-ebs-csi-driver master --squash
